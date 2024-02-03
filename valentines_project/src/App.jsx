22bc023bb17344c667a9a2757bedcd651eabbfe8
m-jewel <wzt206@usask.ca>
2024-02-03 12:48:36 -0600

    added functionality for the YES button

--- valentines_project/src/App.jsx
+++ valentines_project/src/App.jsx
@@ -1,43 +1,65 @@
-import React, {useState} from 'react';
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 import giphy from './assets/giphy.gif';
 import crying from './assets/cry-crying.gif';
+import yesDance from './assets/yesDance.gif';
 
-const phrases = ["Are you sure?", "Pwetty pweases?", "Give me a chance?", "I'm gonna cry...", "Pookie pweases?"]
+const phrases = ["Are you sure?", "Pwetty pweases?", "Give me a chance?", "I'm gonna cry...", "Pookie pweases?"];
+
 function App() {
-    const [count, setCount] = useState(0)
-    const [yesClicked, setYesClicked] = useState(false)
-    const yesBtnSize = count * 20 + 16
-    const [phrase, setPhrase] = useState('');
+  const [phrase, setPhrase] = useState('');
+  const [yesBtnSize, setYesBtnSize] = useState(1);
+  const [showYesDance, setShowYesDance] = useState(false);
+  const [hoverGif, setHoverGif] = useState('');
 
-    function handleNoClick() {
-        setCount(count + 1);
-        setPhrase(getPhrase());
+  function handleYesClick() {
+    setShowYesDance(true);
+    setHoverGif(''); // Reset hover GIF
+    // Optional: Reset the phrase if you don't want it to be displayed after "Yes" is clicked
+    setPhrase('');
+  }
+
+  function handleNoClick() {
+    setPhrase(getPhrase());
+    setYesBtnSize(yesBtnSize + 0.1);
+    setShowYesDance(false); // Hide the yesDance GIF if it's being shown
+  }
+
+  function handleYesHover() {
+    if (!showYesDance) { // Only show hover GIF if the yesDance GIF is not being shown
+      setHoverGif('giphy');
     }
+  }
 
-    function getPhrase() {
-        return phrases[Math.floor(Math.random() * phrases.length)]
+  function handleNoHover() {
+    if (!showYesDance) {
+      setHoverGif('crying');
     }
+  }
 
-    return (
-      <div className="container"> {/* Add the container class */}
+  function handleMouseLeave() {
+    setHoverGif(''); // Hide hover GIFs when not hovering
+  }
+
+  function getPhrase() {
+    return phrases[Math.floor(Math.random() * phrases.length)];
+  }
+
+  return (
+    <div className="container">
+      <div className="text-side">
         <h1>Will you be my pookie for valentines?</h1>
-  
-        <div>
-          <button className="YES">Yes</button>
-          <div className='OMG'>
-            <img src={giphy} alt="Giphy animation"/>
-          </div>
-  
-          <button onClick={handleNoClick} className="NO">No</button>
-          <div className='NYOO'>
-            <img src={crying} alt="Crying animation"/>
-          </div>
-          <span>{phrase}</span>
-        </div>
+        <button className="YES" onMouseEnter={handleYesHover} onMouseLeave={handleMouseLeave} onClick={handleYesClick} style={{ transform: `scale(${yesBtnSize})` }}>Yes</button>
+        <button className="NO" onMouseEnter={handleNoHover} onMouseLeave={handleMouseLeave} onClick={handleNoClick}>No</button>
+        <div>{phrase}</div>
       </div>
-    );
-
+      <div className="gif-side">
+        {hoverGif === 'giphy' && <div className='OMG'><img src={giphy} alt="Giphy animation"/></div>}
+        {hoverGif === 'crying' && <div className='NYOO'><img src={crying} alt="Crying animation"/></div>}
+        {showYesDance && <div className='yesDance'><img src={yesDance} alt="Yes Dance animation"/></div>}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
